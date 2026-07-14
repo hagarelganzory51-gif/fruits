d70a6e1046c3e3d901cbe733a12099f979026960
Hagar Elganzory <hagarelganzory51@gmail.com>
2026-07-14 10:46:43 +0300

    up

--- lib/feature/onboarding/onboarding_screen.dart
+++ lib/feature/onboarding/onboarding_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_app/core/constants/image_app.dart';
+import 'package:fruits_app/core/utils/app_colors.dart';
 import 'package:fruits_app/core/utils/text_styles.dart';
 
 class OnboardingScreen extends StatelessWidget {
@@ -9,15 +11,35 @@ class OnboardingScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
       actions: [
-        TextButton(onPressed: (){},
-        child: Text("Skip",style: TextStyles.style18.copyWith(fontWeight: FontWeight.w400),))
+        Column(
+          children: [
+            TextButton(onPressed: (){},
+            child: Text("Skip",
+            style: TextStyles.style18.copyWith(fontWeight: FontWeight.w400, color: AppColors.darkColor),)),
+          Container(
+          width: 35,
+          height: 2,
+          color: AppColors.darkColor,
+        ),
+          ],
+        ),
       ],
       ),
+      
       body:Column(
         children:[
           Expanded(
             child: PageView.builder(itemBuilder: (context,index){
-              
+              return Column(
+                children: [
+                  SizedBox(height: 12,),
+                  Image.asset(ImageApp.onboarding,height: 300,width: double.infinity,),
+                  SizedBox(height: 20,),
+                  Text("E Shopping",style: TextStyles.style24.copyWith(fontWeight: FontWeight.w600),),
+                  SizedBox(height: 20,),
+                  Text("Explore op organic fruits & grab them",style: TextStyles.style16.copyWith(fontWeight: FontWeight.w400,color: AppColors.darkColor),),
+                ],
+              );
             },
             itemCount: 3,
             ),
