01d0feeddbfb277943839ac59aa5c80c61133fd7
Hagar Elganzory <hagarelganzory51@gmail.com>
2026-08-09 00:17:54 +0300

    up

--- lib/feature/more_screen/more_screen.dart
+++ lib/feature/more_screen/more_screen.dart
@@ -1,10 +1,156 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_app/core/function/navigation.dart' as navigator;
+import 'package:fruits_app/core/utils/app_colors.dart';
+import 'package:fruits_app/core/utils/text_styles.dart';
+import 'package:fruits_app/feature/main_app/main_app_screen.dart';
 
-class MoreScreen extends StatelessWidget {
+class MoreScreen extends StatefulWidget {
   const MoreScreen({super.key});
 
   @override
+  State<MoreScreen> createState() => _MoreScreenState();
+}
+
+class _MoreScreenState extends State<MoreScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: AppColors.whiteColor,
+      appBar: AppBar(
+        backgroundColor: AppColors.whiteColor,
+        elevation: 0,
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
+          onPressed: () {
+            navigator.pushReplacement(context, const MainAPPScreen());
+          },
+        ),
+        title: Text(
+          "Fruit Market",
+          style: TextStyles.style24.copyWith(
+            fontWeight: FontWeight.bold,
+            color: AppColors.primaryColor,
+            fontSize: 22,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Container(
+              width: 90,
+              height: 90,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+                border: Border.all(color: Colors.grey.shade300, width: 1.5),
+              ),
+              child: const Icon(
+                Icons.person_outline,
+                size: 45,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 12),
+            const Text(
+              'Welcome, Fruit Market',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primaryColor,
+                minimumSize: const Size(double.infinity, 45),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                elevation: 0,
+              ),
+              child: const Text(
+                'Login',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            _buildMenuItem(
+              icon: Icons.person_outline,
+              title: 'Profile',
+              onTap: () {},
+            ),
+            _buildMenuItem(
+              icon: Icons.format_list_bulleted,
+              title: 'My Orders',
+              onTap: () {},
+            ),
+            _buildMenuItem(
+              icon: Icons.favorite_border,
+              title: 'Favorite',
+              onTap: () {},
+            ),
+            _buildMenuItem(
+              icon: Icons.language,
+              title: 'Language',
+              onTap: () {},
+            ),
+            _buildMenuItem(
+              icon: Icons.headset_mic_outlined,
+              title: 'Support',
+              onTap: () {},
+            ),
+            _buildMenuItem(
+              icon: Icons.layers_outlined,
+              title: 'Terms & Conditions',
+              onTap: () {},
+            ),
+            _buildMenuItem(
+              icon: Icons.info_outline,
+              title: 'About Us',
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMenuItem({
+    required IconData icon,
+    required String title,
+    required VoidCallback onTap,
+  }) {
+    return Column(
+      children: [
+        ListTile(
+          contentPadding: const EdgeInsets.symmetric(vertical: 2),
+          leading: Icon(icon, color: Colors.black54, size: 22),
+          title: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+              color: Colors.black,
+            ),
+          ),
+          trailing: const Icon(
+            Icons.arrow_forward_ios,
+            size: 14,
+            color: Colors.grey,
+          ),
+          onTap: onTap,
+        ),
+      ],
+    );
   }
 }
